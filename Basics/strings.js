e93e594da1d8ded9d5e1cd5b50a0c1478829f5f1
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2018-12-30 23:27:49 -0500

    Add files via upload

---
+++ Basics/strings.js
@@ -0,0 +1,15 @@
+//Practice
+let name = "Betty Hoo";
+console.log(name);
+
+// Practice
+let firstName = "Meaghan";
+let lastName = "Johns";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Strings and Variables Video Exercise
+let city = "Toronto";
+let country = "Canada";
+let location = city + ", " + country;
+console.log(location);
