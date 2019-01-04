66ce84d0c8b27086fc81b8084b2f46882d0df933
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2019-01-03 19:48:00 -0500

    Add files via upload

---
+++ Basics/boolean.js
@@ -0,0 +1,15 @@
+// Practice Exercise
+
+let temp = 32;
+let isFreezing = temp <= 32;
+
+console.log(isFreezing);
+
+// Challenge: Determine if the patron is a child or a senior based on the given age.
+
+let age = 66;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+
+console.log(isChild);
+console.log(isSenior);
