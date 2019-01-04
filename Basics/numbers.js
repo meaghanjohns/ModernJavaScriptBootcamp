23a7300c3cd8cb82d37b51b842048df271ed47c7
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2019-01-03 19:00:29 -0500

    Add files via upload

---
+++ Basics/numbers.js
@@ -0,0 +1,11 @@
+//Practice
+let age = 28;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
+
+//Numbers Video Challenge Exercise
+let maxScore = 20
+let studentScore = 18
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent);
