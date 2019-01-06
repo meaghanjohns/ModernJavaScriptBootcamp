51e6eba7090c567aa04ad4a002ab8b1af97a288a
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2019-01-06 12:48:02 -0500

    Add files via upload

---
+++ Basics/scope.js
@@ -0,0 +1,21 @@
+// Practice exercise.
+
+let varOne = "varOne";
+
+if (true) {
+    console.log(varOne);
+    let varTwo = "varTwo";
+    console.log(varTwo);
+
+    if (true) {
+        let varFour = "varFour";
+        console.log("varFour");
+    };
+};
+
+if (true) {
+    let varThree = "varThree";
+    console.log(varThree);
+};
+
+console.log(varTwo);
